62e2f2de0efbe424423116486c6a393ae3cd85c8
jhanmenly <jhanmenlynicasioventura@gmail.com>
2025-05-03 11:22:00 -0500

    doctor

---
+++ app/Models/Medico.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medico extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'especialidad',
+        'telefono',
+        'email',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
